30d48df1f6083ec7a6a76784129a2baf5d8e1005
Arif Ansari <arifansari729878@gmail.com>
2023-10-05 11:51:11 +0530

    commit

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -9,12 +9,41 @@ const Navbar = () => {
     const pathName = usePathname()
     return (
         <>
-            <div className='w-full bg-white h-20 px-8 md:px-4 flex md:justify-evenly justify-between items-center top-0 sticky z-10'>
+            <div className='nav_container'>
                 <div>
                     <Link href='/'><h1 className='text-3xl font-bold text-[#8fab3a]'>Portfolio</h1></Link>
                 </div>
-                <ul className=' navBar'>
-                <li className='hover:text-[#8fab3a]'>
+                <ul className='navBar'>
+                    <li >
+                        <Link href='/' className={`link ${pathName === '/' ? 'text-[#8fab3a] font-bold' : ''}`} >Home</Link>
+                    </li>
+                    <li >
+                        <Link href='/skills' className={`link ${pathName === '/skills' ? 'text-[#8fab3a] font-bold' : ''}`}>Skills</Link>
+                    </li>
+                    <li >
+                        <Link href='/education' className={`link ${pathName === '/education' ? 'text-[#8fab3a] font-bold' : ''}`}>Education</Link>
+                    </li>
+                    <li >
+                        <Link href='/projects' className={`link ${pathName === '/projects' ? 'text-[#8fab3a] font-bold' : ''}`}>Projects</Link>
+                    </li>
+                    <li >
+                        <Link href='/about' className={`link ${pathName === '/about' ? 'text-[#8fab3a] font-bold' : ''}`}>About</Link>
+                    </li>
+                    <li>
+                        <Link href='/contact' className={`link ${pathName === '/contact' ? 'text-[#8fab3a] font-bold' : ''}`}>Contact</Link>
+                    </li>
+                </ul>
+                <div className='menu-btn-container'>
+                    {
+                        toggle ? <button onClick={() => setToggle(!toggle)}><AiOutlineClose /></button>
+                            :
+                            <button onClick={() => setToggle(!toggle)}><AiOutlineMenu /></button>
+                    }
+                </div>
+
+                {/* Responsive Menu  */}
+                <ul className={`${toggle ? "responsive-menubar" : "responsive-menubar-hide"}`}>
+                    <li className='hover:text-[#8fab3a]'>
                         <Link href='/' className={`link ${pathName === '/' ? 'text-[#8fab3a] font-bold' : ''}`} >Home</Link>
                     </li>
                     <li className='hover:text-[#8fab3a]'>
@@ -33,35 +62,6 @@ const Navbar = () => {
                         <Link href='/contact' className={`link ${pathName === '/contact' ? 'text-[#8fab3a] font-bold' : ''}`}>Contact</Link>
                     </li>
                 </ul>
-                {/* <div className='hover:text-[#8fab3a] md:hidden item-center pl-3 font-bold text-3xl'>
-                    {
-                        toggle ? <button onClick={() => setToggle(!toggle)}><AiOutlineClose /></button>
-                            :
-                            <button onClick={() => setToggle(!toggle)}><AiOutlineMenu /></button>
-                    }
-                </div> */}
-
-                {/* Responsive Menu  */}
-                {/* <ul className={`top-[52px] ${toggle ? 'left-0' : 'left-[-100%]'} gap-5 duration-500 bg-black  text-white font-semibold absolute flex text-center pt-6 mt-[28px] flex-col h-screen w-3/4 md:hidden`}>
-                    <li className='hover:text-[#8fab3a]'>
-                        <Link href='/' className={`link ${pathName === '/' ? 'text-[#8fab3a] font-bold' : ''}`} >Home</Link>
-                    </li>
-                    <li className='hover:text-[#8fab3a]'>
-                        <Link href='/skills' className={`link ${pathName === '/skills' ? 'text-[#8fab3a] font-bold' : ''}`}>Skills</Link>
-                    </li>
-                    <li className='hover:text-[#8fab3a]'>
-                        <Link href='/education' className={`link ${pathName === '/education' ? 'text-[#8fab3a] font-bold' : ''}`}>Education</Link>
-                    </li>
-                    <li className='hover:text-[#8fab3a]'>
-                        <Link href='/projects' className={`link ${pathName === '/projects' ? 'text-[#8fab3a] font-bold' : ''}`}>Projects</Link>
-                    </li>
-                    <li className='hover:text-[#8fab3a]'>
-                        <Link href='/about' className={`link ${pathName === '/about' ? 'text-[#8fab3a] font-bold' : ''}`}>About</Link>
-                    </li>
-                    <li className='hover:text-[#8fab3a]'>
-                        <Link href='/contact' className={`link ${pathName === '/contact' ? 'text-[#8fab3a] font-bold' : ''}`}>Contact</Link>
-                    </li>
-                </ul> */}
             </div>
         </>
     )
