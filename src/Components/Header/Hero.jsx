52be25c97381dbdb305f90068d7ae53cfee9ddbf
Arif Ansari <arifansari729878@gmail.com>
2023-10-05 12:20:18 +0530

    commit

--- src/Components/Header/Hero.jsx
+++ src/Components/Header/Hero.jsx
@@ -6,10 +6,10 @@ import Links from './Links';
 const Hero = () => {
     return (
         <>
-            <div className='py-20 overflow-hidden flex flex-col-reverse  gap-12 md:gap-10 md:flex-row flex-wrap items-center justify-center'>
+            <div className='custom-hero-css py-20 overflow-hidden flex flex-col-reverse  gap-12 md:gap-10 md:flex-row flex-wrap items-center justify-center'>
                 <div className='text-center profile_pic'>
                 </div>
-                <div className='hidden md:flex'>
+                <div className='links-hero'>
                     <Links />
                 </div>
                 <div className='w-[100%] md:w-[40%]'>
