ec5e7a2668c905b6e70822ed8ad41fdd9e7927d3
Arif Ansari <arifansari729878@gmail.com>
2023-10-05 16:02:12 +0530

    commit

--- src/Components/SectionDivider.jsx
+++ src/Components/SectionDivider.jsx
@@ -5,7 +5,7 @@ const SectionDivider = ({ title, subTitle }) => {
         <>
             <div className='section-devider-custome'>
                 <div className='text-center mb-10 md:mb-16'>
-                    <h1 className='section-devider-title'>{title}</h1>
+                    <h1 className='text-2xl md:text-4xl font-bold my-2'>{title}</h1>
                     <div className='section-devider-line'></div>
                     <p className='text-xl'>{subTitle}</p>
                 </div>
