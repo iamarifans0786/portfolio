1281f54675e65b05cd94da28eaec99cc96c534c1
Arif Ansari <arifansari729878@gmail.com>
2023-10-05 13:53:50 +0530

    commit

--- src/Components/Header/Hero.jsx
+++ src/Components/Header/Hero.jsx
@@ -6,7 +6,7 @@ import Links from './Links';
 const Hero = () => {
     return (
         <>
-            <div className='custom-hero-css flex flex-col-reverse lg:flex-row justify-center items-center lg:justify-evenly border-4 my-2 lg:my-10 border-red-600 '>
+            <div className='custom-hero-css flex flex-col-reverse lg:flex-row justify-center items-center lg:justify-evenly border-4  lg:my-10 border-red-600 '>
                 <div className='img-sec text-center profile_pic'>
                 </div>
                 <div className='hero-content-sec'>
